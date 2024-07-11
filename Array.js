6a73f8f9a74d6f7d9f5631e55a866469157d1d04
Abhinish Kumar <141950751+Abhinish-Kumar@users.noreply.github.com>
2024-07-11 15:13:07 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -1,1 +1,12 @@
 
+//1. write append function to append an element with time complexity O(1)
+      let arr = [1, 2, 3, 4];
+      //O(n)
+      function append(element) {
+        arr[arr.length] = element;
+      }
+
+      append(5);
+      append(6);
+      console.log(arr);
+//==>>  [1, 2, 3, 4, 5, 6]
