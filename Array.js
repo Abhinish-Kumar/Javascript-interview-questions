f43edfeedc55d867400e5e278dbb7e86ee591f87
Abhinish Kumar <141950751+Abhinish-Kumar@users.noreply.github.com>
2024-07-11 15:29:51 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -10,3 +10,29 @@
       append(6);
       console.log(arr);
 //==>>  [1, 2, 3, 4, 5, 6]
+
+
+//2.  
+//insert the element in an array at a given index in an array by shifting the elements from back
+      //min(1)  max(n)
+      let arr = [1, 2, 3, 4, 5];
+      function insert(index, element) {
+        for (let i = arr.length; i > index; i--) {
+          arr[i] = arr[i - 1];
+        }
+        arr[index] = element;
+      }
+
+      insert(2, 50000);
+      console.log(arr);
+
+//==> [1, 2, 50000, 3, 4, 5]
+
+
+
+
+
+
+
+
+
